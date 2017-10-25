2395e739efbb5fa0d7e5edf1ef3d017aeec6ca88
Marouane Bouhouch <marouanebhch@gmail.com>
2017-10-25 21:40:47 +0100

    Main changements

--- src/ClientRemote.java
+++ src/ClientRemote.java
@@ -3,34 +3,43 @@ import metier.entities.Compte;
 
 import javax.naming.Context;
 import javax.naming.InitialContext;
-import javax.naming.NamingException;
+import java.util.Date;
 import java.util.List;
 
 public class ClientRemote {
     public static void main(String[] args) {
         try {
             Context ctx = new InitialContext();
-            String appName = "";
-            String moduleName = "Banque";
+            String appName = "BanqueWeb";
+            String moduleName = "BanqueWeb";
             String beanName = "BK";
             String remoteInterface = BanqueRemote.class.getName();
             String name = "ejb:" + appName + "/" + moduleName + "/" + beanName + "!" + remoteInterface;
 
             BanqueRemote proxy = (BanqueRemote) ctx.lookup(name);
-/*
-            proxy.addCompte(new Compte());
-            proxy.addCompte(new Compte());
-            proxy.addCompte(new Compte());*/
 
-            Compte cp=proxy.getCompte(1L);
+            Compte c1 = new Compte();
+            c1.setDate_creation(new Date());
+            Compte c2 = new Compte();
+            c2.setDate_creation(new Date());
+            Compte c3 = new Compte();
+            c3.setDate_creation(new Date());
+            c3.setSolde(7000);
+
+            proxy.addCompte(c1);
+            proxy.addCompte(c2);
+            proxy.addCompte(c3);
+
+            Compte cp = proxy.getCompte(1L);
             System.out.println(cp.getSolde());
 
             proxy.verser(1L, 4000);
             proxy.retirer(1L, 2000);
             proxy.virement(1L,2L ,1000);
+
             List<Compte> cptes = proxy.listComptes();
             for(Compte c:cptes){
-                System.out.println(c.getCode()+":"+c.getSolde());
+                System.out.println(c.getCode() + " : " + c.getSolde());
             }
 
         } catch (Exception e) {
